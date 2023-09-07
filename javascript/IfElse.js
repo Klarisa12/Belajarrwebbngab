03c10b32823bbf76863acb3517d2173668498b49
Klarisa12 <Klarisa2988@gmail.com>
2023-09-07 12:22:23 +0700

    seconddd

---
+++ javascript/IfElse.js
@@ -0,0 +1,13 @@
+const angka = 100
+
+if(angka > 0) {
+    console.log(`${angka} adalah bilangan positif`)
+}
+
+else if(angka < 0) {
+    console.log(`${angka} adalah bilangan negatif`)
+}
+
+else {
+    console.log(`${angka} adalah bilangan nol`)
+}
